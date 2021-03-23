b9ead206d2d010ba9d70b5c701226a61c8e5168e
sinchanalahari <sinchanalahari@gmail.com>
2021-03-23 12:54:47 +0530

    created home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,10 +21,11 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+        <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color:#3498db">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
+
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -49,6 +50,15 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Chat</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Mypost</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Profile</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
